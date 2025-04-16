a7652f5d4e35bb86b08b2dc2b42ab06159ca6b95
yntymakdev <kubanychevyntymak21@gmail.com>
2025-04-16 09:40:09 +0600

    changed the visivility

--- player.py
+++ player.py
@@ -3,6 +3,9 @@ from pygame.locals import *
 
 pygame.init()
 
+clock = pygame.time.Clock()
+fps = 60
+
 screen_width = 1000
 screen_height = 1000
 
@@ -20,29 +23,65 @@ bg_img = pygame.image.load('img/sky.png')
 
 class Player():
 	def __init__(self, x, y):
-		img = pygame.image.load('img/guy1.png')
-		self.image = pygame.transform.scale(img, (40, 80))
+		self.images_right = []
+		self.images_left = []
+		self.index = 0
+		self.counter = 0
+		for num in range(1, 5):
+			img_right = pygame.image.load(f'img/guy{num}.png')
+			img_right = pygame.transform.scale(img_right, (40, 80))
+			img_left = pygame.transform.flip(img_right, True, False)
+			self.images_right.append(img_right)
+			self.images_left.append(img_left)
+		self.image = self.images_right[self.index]
 		self.rect = self.image.get_rect()
 		self.rect.x = x
 		self.rect.y = y
+		self.width = self.image.get_width()
+		self.height = self.image.get_height()
 		self.vel_y = 0
 		self.jumped = False
+		self.direction = 0
 
 	def update(self):
 		dx = 0
 		dy = 0
+		walk_cooldown = 5
 
 		#get keypresses
 		key = pygame.key.get_pressed()
 		if key[pygame.K_SPACE] and self.jumped == False:
-			self.vel_y = -15
+			self.vel_y = -10
 			self.jumped = True
 		if key[pygame.K_SPACE] == False:
 			self.jumped = False
 		if key[pygame.K_LEFT]:
 			dx -= 5
+			self.counter += 1
+			self.direction = -1
 		if key[pygame.K_RIGHT]:
 			dx += 5
+			self.counter += 1
+			self.direction = 1
+		if key[pygame.K_LEFT] == False and key[pygame.K_RIGHT] == False:
+			self.counter = 0
+			self.index = 0
+			if self.direction == 1:
+				self.image = self.images_right[self.index]
+			if self.direction == -1:
+				self.image = self.images_left[self.index]
+
+
+		#handle animation
+		if self.counter > walk_cooldown:
+			self.counter = 0
+			self.index += 1
+			if self.index >= len(self.images_right):
+				self.index = 0
+			if self.direction == 1:
+				self.image = self.images_right[self.index]
+			if self.direction == -1:
+				self.image = self.images_left[self.index]
 
 
 		#add gravity
@@ -52,6 +91,23 @@ class Player():
 		dy += self.vel_y
 
 		#check for collision
+		for tile in world.tile_list:
+			#check for collision in x direction
+			if tile[1].colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
+				dx = 0
+			#check for collision in y direction
+			if tile[1].colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
+				#check if below the ground i.e. jumping
+				if self.vel_y < 0:
+					dy = tile[1].bottom - self.rect.top
+					self.vel_y = 0
+				#check if above the ground i.e. falling
+				elif self.vel_y >= 0:
+					dy = tile[1].top - self.rect.bottom
+					self.vel_y = 0
+
+
+
 
 		#update player coordinates
 		self.rect.x += dx
@@ -63,6 +119,7 @@ class Player():
 
 		#draw player onto screen
 		screen.blit(self.image, self.rect)
+		pygame.draw.rect(screen, (255, 255, 255), self.rect, 2)
 
 
 
@@ -99,6 +156,7 @@ class World():
 	def draw(self):
 		for tile in self.tile_list:
 			screen.blit(tile[0], tile[1])
+			pygame.draw.rect(screen, (255, 255, 255), tile[1], 2)
 
 
 
@@ -132,6 +190,8 @@ world = World(world_data)
 
 run = True
 while run:
+
+	clock.tick(fps)
 
 	screen.blit(bg_img, (0, 0))
 	screen.blit(sun_img, (100, 100))
